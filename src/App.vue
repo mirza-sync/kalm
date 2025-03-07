dd7cda1e157d82f858b5244a6171ecc739cac78d
mirza-sync <mirzasyahmi123@gmail.com>
2025-03-07 23:57:35 +0800

    Add breath animation

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,36 @@
 </script>
 
 <template>
-  <div>I am Kalm</div>
+  <div>
+    <h1 id="title">I am Kalm</h1>
+    <div id="breath"></div>
+  </div>
 </template>
 
 <style scoped>
+#title {
+  text-align: center;
+}
+
+#breath {
+  width: 300px;
+  height: 300px;
+  background: aqua;
+  border-radius: 50%;
+  animation-duration: 3s;
+  animation-name: breath-in-out;
+  animation-iteration-count: infinite;
+  animation-direction: alternate;
+  animation-timing-function: ease-in-out;
+}
+
+@keyframes breath-in-out {
+  from {
+    scale: 0%;
+  }
+
+  to {
+    scale: 100%;
+  }
+}
 </style>
